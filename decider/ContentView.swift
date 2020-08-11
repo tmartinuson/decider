944a85dccd734fca8f244abe30b94a3bf3e6f670
Tristan Martinuson <tmartinuson@gmail.com>
2020-08-10 19:23:36 -0500

    Initial Commit

---
+++ decider/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  decider
+//
+//  Created by Tristan Martinuson on 2020-08-10.
+//  Copyright © 2020 Tristan Martinuson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
